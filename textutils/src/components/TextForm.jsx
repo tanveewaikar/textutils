0f1e14708ce7e197b002100d4534295a0e07944c
Tanvee Mohan Waikar <tanveewaikar1311@gmail.com>
2025-12-20 22:20:50 +0530

    Textutils advanced features addede(wordCount, sentences, paragraph count, reading time )

--- textutils/src/components/TextForm.jsx
+++ textutils/src/components/TextForm.jsx
@@ -27,7 +27,16 @@ export default function TextForm(props) {
     const[text, setText] = useState("")
     const [preview, setPreview] = useState("");
     
-    
+    const wordCount = text.trim()=== "" ? 0 : text.trim().split(/\s+/).length;
+    const charaWithoutSpace = text.replace(/\s/g,"").length;
+    const sentences = text.trim()=== "" ? 0 : text.split(/[!?.]+/).filter(Boolean).length;
+    const para = text.trim()==="" ? 0 : text.split(/\n+/).filter(p=> p.trim()!=="").length;
+    const CharaWithSpaces = text.length;
+    const secondsPerWord = 0.3;
+    const totalSeconds = Math.round(wordCount*secondsPerWord);
+    const hour = Math.floor(totalSeconds/3600);
+    const min = Math.floor((totalSeconds % 3600)/60)
+    const sec = Math.floor(totalSeconds % 60);
   return (
     <>
       <div className='container'>
@@ -38,10 +47,35 @@ export default function TextForm(props) {
          <button type="button" className="btn btn-primary mx-1" onClick={upperCaseText}>Convert to uppercase</button>
          <button type="button" className="btn btn-primary mx-1" onClick={lowerCaseText}>Convert to lowercase</button>
       </div>
+
+      <div className='container mt-4' >
+      <h2>Your Text summary</h2>
+      <div className='container' style={{display:"flex", gap:"40px", marginTop:"20px"}}>
+      <div>
+        <h2>{wordCount}</h2>
+        <p>Words</p>
+      </div>
+      <div>
+        <h2>{charaWithoutSpace}</h2>
+        <p>Characters</p>
+      </div>
+      <div>
+        <h2>{sentences}</h2>
+        <p>Sentence</p>
+      </div>
+      <div>
+        <h2>{para}</h2>
+        <p>Paragraph</p>
+      </div>
+      </div>
+      <hr />
+      <p>Characters with spaces : <strong>{CharaWithSpaces}</strong></p>
+      <p>Reading time :<strong> {hour}h {min}m {sec}s</strong> </p>
+      </div>
+
       <div className='container my-4'>
-        <h2>Your Text summary</h2>
-        <p>{text.split(" ").length} Words {text.length} Characters</p>
-        <p>{0.008 * text.split(" ").length} Minitues to read </p>
+        {/* <p>{text.split(" ").length} Words {text.length} Characters</p>
+        <p>{0.008 * text.split(" ").length} Minitues to read </p> */}
         <button type="button" className="btn btn-primary mx-1" onClick={previewText}>Preview</button>
         {preview.length > 0 && (
          <>
